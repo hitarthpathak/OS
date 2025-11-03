1faa99ad7e113eb870a1635718f9800b05230cc6
Hitarth Pathak <163673363+hitarthpathak@users.noreply.github.com>
2025-11-03 13:41:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let lock_screen = document.getElementById("lock-screen");
+let form = document.getElementById("form");
+let password = document.getElementById("password");
+
+// ------------------------------------------------------------------------------------------------
+
+function check_login(event) {
+
+    event.preventDefault();
+
+    if (password.value == "test") {
+        login();
+    }
+    else {
+        wrong_password();
+    }
+
+};
+
+// ------------------------------------------------------------------------------------------------
+
+function login() {
+
+    lock_screen.style.animation = "show-home-screen 0.5s";
+
+    setTimeout(() => {
+
+        lock_screen.style.display = "none";
+
+        location = "Desktop.html";
+
+    }, 400);
+
+};
+
+// ------------------------------------------------------------------------------------------------
+
+function wrong_password() {
+
+    form.style.removeProperty("animation");
+
+    form.offsetWidth;
+
+    form.style.animation = "show-error 0.3s";
+
+};
